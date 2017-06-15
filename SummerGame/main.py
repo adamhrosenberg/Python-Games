fdadde03f3a92118fee7631964e6aa9f224bf16d
Adam <adam.arosenberg@gmail.com>
2017-06-15 14:32:23 -0600

    code cleaned up and sprite / settings files added

--- SummerGame/main.py
+++ SummerGame/main.py
@@ -1,13 +1,65 @@
 import pygame
+import random
+from settings import *
+from sprite import *
 
-if (__name__ == "__main__"):
-    pygame.init()
-    window_size = window_width, window_height = 640, 480
-    gameWindow = pygame.display.set_mode(window_size)
+class Game:
+    def __init__(self):
+        # initialize game window, etc
+        pygame.init()
+        pygame.mixer.init()
+        self.screen = pygame.display.set_mode((WIDTH, HEIGHT))
+        self.clock = pygame.time.Clock()
+        self.running = True
 
-    running = 1
+    def new(self):
+        # start a new game
+        self.all_sprites = pygame.sprite.Group()
+        self.player = Player()
+        self.all_sprites.add(self.player)
+        self.run()
 
-    while running:
+    def run(self):
+        # Game Loop
+        self.playing = True
+        while self.playing:
+            self.clock.tick(FPS)
+            self.events()
+            self.update()
+            self.draw()
+
+    def update(self):
+        # Game Loop - Update
+        self.all_sprites.update()
+
+    def events(self):
+        # Game Loop - events
         for event in pygame.event.get():
+            # check for closing window
             if event.type == pygame.QUIT:
-                running = 0
+                if self.playing:
+                    self.playing = False
+                self.running = False
+
+    def draw(self):
+        # Game Loop - draw
+        self.screen.fill(BLACK)
+        self.all_sprites.draw(self.screen)
+        # *after* drawing everything, flip the display. double buffering
+        pygame.display.flip()
+
+    def show_start_screen(self):
+        # game splash/start screen
+        pass
+
+    def show_go_screen(self):
+        # game over/continue
+        pass
+
+g = Game()
+g.show_start_screen()
+while g.running:
+    g.new()
+    g.show_go_screen()
+
+pygame.quit()
